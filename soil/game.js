a9ee8354924a9c4b44b473f33f6e06675335c504
Andrey Leshenko <andrey.leshenko@gmail.com>
2018-05-25 00:14:35 +0300

    Work on soil

--- soil/game.js
+++ soil/game.js
@@ -8,13 +8,12 @@ let soil = new Array(W * H);
 soil.fill(0);
 let imageData = ctx.createImageData(W, H);
 
-for (let y = H - 20; y < H; y++) {
+for (let y = H - 30; y < H; y++) {
 	for (let x = 0; x < W; x++) {
 		soil[lin(x, y)] = 255;
 		imageData.data[lin(x, y) * 4 + 3] = 255;
 	}
 }
-
 function lin(x, y) {
 	return y * W + x;
 }
@@ -26,21 +25,24 @@ var px = [];
 var py = [];
 var vx = [];
 var vy = [];
+var anti = [];
 
 var mouseX = 0;
 var mouseY = 0;
 var mouseDown = false;
+var antiOn = false;
 let keys = {};
+let burstSize = 40;
+let fireSpeed = 20;
+let antiLife = 2;
 
 function update() {
 	if (mouseDown) {
-		let burstSize = 40;
 		for (let i = 0; i < burstSize; i++) {
 			let dx = mouseX - cx;
 			let dy = mouseY - cy;
 			let dist = Math.sqrt(dx * dx + dy * dy) + 1;
 
-			let fireSpeed = 20;
 			let randomness = 3;
 
 			px.push(cx);
@@ -49,6 +51,7 @@ function update() {
 			vy.push(dy / dist * fireSpeed + (Math.random() - 0.5) * randomness);
 			px[px.length - 1] += vx[vx.length - 1] * Math.random();
 			py[py.length - 1] += vy[vy.length - 1] * Math.random();
+			anti.push(antiOn ? antiLife : 0);
 		}
 	}
 
@@ -67,11 +70,22 @@ function update() {
 				destroyed = true;
 				break;
 			}
-			if (soil[lin(x, y)] != 0) {
-				soil[lin(prevX, prevY)] = 255;
-				imageData.data[lin(prevX, prevY) * 4 + 3] = 255;
-				destroyed = true;
-				break;
+			if (y > 0 && soil[lin(x, y)] != 0) {
+				if (!anti[i]) {
+					soil[lin(prevX, prevY)] = 255;
+					imageData.data[lin(prevX, prevY) * 4 + 3] = 255;
+					destroyed = true;
+					break;
+				}
+				else {
+					soil[lin(x, y)] = 0;
+					imageData.data[lin(x, y) * 4 + 3] = 0;
+					anti[i]--;
+					if (!anti[i]) {
+						destroyed = true;
+						break;
+					}
+				}
 			}
 
 			prevX = x;
@@ -83,6 +97,7 @@ function update() {
 			py.splice(i, 1);
 			vx.splice(i, 1);
 			vy.splice(i, 1);
+			anti.splice(i, 1);
 			continue;
 		}
 
@@ -115,10 +130,29 @@ function update() {
 
 	ctx.putImageData(imageData, 0, 0);
 	ctx.strokeRect(cx - 6, cy - 6, 13, 13);
+	if (false)
+	{
+		ctx.lineCap = 'round';
+		ctx.beginPath();
+		ctx.arc(cx, cy, 7, 0, 2 * Math.PI);
+		ctx.stroke();
+
+		let dx = mouseX - cx;
+		let dy = mouseY - cy;
+		let dist = Math.sqrt(dx * dx + dy * dy) + 1e-6;
+		ctx.beginPath();
+		ctx.moveTo(cx, cy);
+		ctx.lineTo(cx + dx / dist * 9, cy + dy / dist * 9);
+		ctx.lineWidth = 3;
+		ctx.stroke();
+		ctx.lineWidth = 2;
+	}
 
 	for (var i = 0; i < px.length; i++) {
 		ctx.fillRect(px[i], py[i], 1, 1);
 	}
+
+	ctx.fillText((antiOn ? '-' : '+') + ' count: ' + burstSize + ' speed: ' + fireSpeed, 10, 10);
 
 	setTimeout(update, 16);
 
@@ -154,7 +188,6 @@ function update() {
 		vx[i] *= 0.93;
 		vy[i] *= 0.93;
 	}
-
 	*/
 }
 
@@ -179,6 +212,32 @@ function onKeyEvent(e) {
 		keys[e.key] = true;
 	else if (e.type == 'keyup')
 		keys[e.key] = false;
+
+	if (e.type == 'keydown') {
+		if (e.key == 'q') {
+			antiOn = !antiOn;
+		}
+		if (e.key == 'c') {
+			for (let y = H - 300; y < H; y++) {
+				for (let x = W * 0.25; x < W * 0.75; x++) {
+					soil[lin(x, y)] = 255;
+					imageData.data[lin(x, y) * 4 + 3] = 255;
+				}
+			}
+		}
+		if (e.key == 'j') {
+			burstSize = Math.max(20, burstSize - 10);
+		}
+		if (e.key == 'k') {
+			burstSize += 10;
+		}
+		if (e.key == 'h') {
+			fireSpeed = Math.max(10, fireSpeed - 5);
+		}
+		if (e.key == 'l') {
+			fireSpeed += 5;
+		}
+	}
 }
 
 document.addEventListener('mousedown', onMouseEvent);
